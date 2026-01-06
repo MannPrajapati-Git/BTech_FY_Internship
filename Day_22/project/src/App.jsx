697ab70405173c196cc872e54edf1330bdefaa30
Mann Prajapati <mannprajapati2502@gmail.com>
2026-01-06 16:58:48 +0530

    Day 22 completed successfully.

---
+++ Day_22/project/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import TodoList from './components/TodoList'
+import AddTodo from './components/AddTodo'
+
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ <TodoList /> }/>
+          <Route path="/addTodo" element={<AddTodo />}/>
+        </Routes>
+      </BrowserRouter>
+
+    </>
+  )
+}
+
+export default App
